e2b61ed96b7241c92885e81c7e6641a5bd159a62
yowsean <yowseanli@gmail.com>
2017-11-19 15:03:16 -0800

    switch to ggvis

--- hw04/app/gradevis.R
+++ hw04/app/gradevis.R
@@ -7,7 +7,7 @@
 # Date: 11-13-2017
 # ===================================================================
 library(shiny)
-library(ggplot2)
+library(ggvis)
 
 # Define UI ----
 ui <- fluidPage(
@@ -65,10 +65,10 @@ ui <- fluidPage(
       # Output: Tabset w/ barchart, histogram, scatterplot ----
       tabsetPanel(
         tabPanel("Barchart",
-                 plotOutput("gbar")
+                 ggvisOutput("gbar")
         ),
         tabPanel("Histogram",
-                 plotOutput("hist"),
+                 ggvisOutput("hist"),
                  h5("Summary Statistics"),
                  verbatimTextOutput("summary")
         ),
@@ -104,7 +104,7 @@ server <- function(input, output) {
   })
   
   # Barchart of grades
-  output$gbar <- renderPlot({
+  barchart <- reactive({
     dat <- grade_data()
     tbl <- as.data.frame(table(dat$Grade))
     colnames(tbl)[1] <- 'Grade'
@@ -114,22 +114,32 @@ server <- function(input, output) {
                                     "C+","C", "C-",
                                     "D", "F"))
     tbl <- tbl[order(tbl$Grade),]
-    ggplot(tbl, aes(x=Grade, y=Freq)) +
-      geom_bar(stat='identity', alpha="0.75", fill = "#2196F3") +
-      xlab('Grade') +
-      scale_y_continuous(name="Frequency", breaks = seq(0, 55, 5), limits=c(0, 55)) +
-      theme_minimal()
+    tbl %>%
+      ggvis(~Grade, ~Freq, fill := "#2196F3", opacity := 0.75) %>%
+      add_axis("y", title = "frequency") %>%
+      layer_bars()
+    #ggplot(tbl, aes(x=Grade, y=Freq)) +
+    #  geom_bar(stat='identity', alpha="0.75", fill = "#2196F3") +
+    #  xlab('Grade') +
+    #  scale_y_continuous(name="Frequency", breaks = seq(0, 55, 5), limits=c(0, 55)) +
+    #  theme_minimal()
   })
+  barchart %>% bind_shiny("gbar")
   
   # Histogram for selected variable
-  output$hist <- renderPlot({
+  histogram <- reactive({
     dat <- grade_data()
-    ggplot(dat, aes_string(x=input$xvar)) +
-      geom_histogram(binwidth = input$bin, fill="#9E9E9E", col="white") +
-      scale_x_continuous(name=input$xvar, breaks = seq(0, 110, input$bin), limits=c(0, 110)) +
-      scale_y_continuous(name="count", breaks = seq(0, 130, 20), limits=c(0, 130)) +
-      theme_minimal()
+    xvar <- prop("x", as.symbol(input$xvar))
+    dat %>%
+      ggvis(x=xvar, fill :="#9E9E9E", opacity := 0.75) %>%
+      layer_histograms(width = input$bin)
+    #ggplot(dat, aes_string(x=input$xvar)) +
+    #  geom_histogram(binwidth = input$bin, fill="#9E9E9E", col="white") +
+    #  scale_x_continuous(name=input$xvar, breaks = seq(0, 110, input$bin), limits=c(0, 110)) +
+    #  scale_y_continuous(name="count", breaks = seq(0, 130, 20), limits=c(0, 130)) +
+    #  theme_minimal()
   })
+  histogram %>% bind_shiny("hist")
   
   # Summary of selected variable
   output$summary <- renderPrint({
@@ -139,15 +149,32 @@ server <- function(input, output) {
   })
   
   # Scatterplot of 2 variables
-  output$scatter <- renderPlot({
-    dat <- grade_data()
-    ggplot(dat, aes_string(x=input$xvar2, y=input$yvar2)) +
-      geom_point(alpha=input$opacity, size=3) +
-      geom_smooth(method=input$line) +
-      scale_x_continuous(name=input$xvar2, breaks = seq(0, 100, 10), limits=c(0, 100)) +
-      scale_y_continuous(name=input$yvar2, breaks = seq(0, 100, 10), limits=c(0, 100)) +
-      theme_minimal()
+  scatterplot <- reactive({
+    dat2 <- grade_data()
+    dat2 <- dat2[,c(input$xvar2,input$yvar2)]
+    colnames(dat2) <- c("x", "y")
+    if (input$line=="none"){
+      dat2 %>%
+        ggvis(~x, ~y, opacity := input$opacity) %>%
+        add_axis("x", title = input$xvar2) %>%
+        add_axis("y", title = input$yvar2) %>%
+        layer_points()
+    } else {
+      dat2 %>%
+        ggvis(~x, ~y, opacity := input$opacity) %>%
+        add_axis("x", title = input$xvar2) %>%
+        add_axis("y", title = input$yvar2) %>%
+        layer_points() %>%
+        layer_model_predictions(model=input$line)
+    }
+    #ggplot(dat, aes_string(x=input$xvar2, y=input$yvar2)) +
+    #  geom_point(alpha=input$opacity, size=3) +
+    #  geom_smooth(method=input$line) +
+    #  scale_x_continuous(name=input$xvar2, breaks = seq(0, 100, 10), limits=c(0, 100)) +
+    #  scale_y_continuous(name=input$yvar2, breaks = seq(0, 100, 10), limits=c(0, 100)) +
+    #  theme_minimal()
   })
+  scatterplot %>% bind_shiny("scatter")
   
   # Correlation coefficient of 2 variables
   output$corr <- renderText({
--- hw04/code/tests.R
+++ hw04/code/tests.R
@@ -6,7 +6,8 @@
 # ===================================================================
 
 # Test remove_missing()
-test_that("remove_missing", {
+context("remove_missing")
+test_that("remove_missing works", {
   expect_equal(remove_missing(c(1, 2, 3, NA, 4)), c(1, 2, 3, 4))
   expect_equal(remove_missing(c(1, 2, 3, 4)), c(1, 2, 3, 4))
   expect_equal(remove_missing(c(NA, NA, NA, NA)), c())
@@ -14,7 +15,8 @@ test_that("remove_missing", {
 })
 
 # Test get_minimum()
-test_that("get_minimum", {
+context("get_minimum")
+test_that("get_minimum works", {
   expect_equal(get_minimum(c(5, 2, 1, NA, 4)), 1)
   expect_equal(get_minimum(c(9, 2, 14, 4)), 2)
   expect_equal(get_minimum(c(NA, NA, 6, NA)), 6)
@@ -22,7 +24,8 @@ test_that("get_minimum", {
 })
 
 # Test get_maximum()
-test_that("get_maximum", {
+context("get_maximum")
+test_that("get_maximum works", {
   expect_equal(get_maximum(c(3, 2, 3, NA, 4)), 4)
   expect_equal(get_maximum(c(5, 11, 3, 4)), 11)
   expect_equal(get_maximum(c(NA, NA, 6, NA)), 6)
@@ -30,7 +33,8 @@ test_that("get_maximum", {
 })
 
 # Test get_range()
-test_that("get_range", {
+context("get_range")
+test_that("get_range works", {
   expect_equal(get_range(c(1, 2, 3, NA, 4)), 3)
   expect_equal(get_range(c(5, 2, 3, 4)), 3)
   expect_equal(get_range(c(NA, 0, 6, NA)), 6)
@@ -38,7 +42,8 @@ test_that("get_range", {
 })
 
 # Test get_percentile10()
-test_that("get_percentile10", {
+context("get_percentile10")
+test_that("get_percentile10 works", {
   expect_equal(get_percentile10(c(1, 2, 3, NA, 4)), 1.3)
   expect_equal(get_percentile10(c(5, 2, 13, 2, 4)), 2)
   expect_equal(get_percentile10(c(NA, 10, NA)), 10)
@@ -46,7 +51,8 @@ test_that("get_percentile10", {
 })
 
 # Test get_percentile90()
-test_that("get_percentile90", {
+context("get_percentile90")
+test_that("get_percentile90 works", {
   expect_equal(get_percentile90(c(1, 2, 3, NA, 4)), 3.7)
   expect_equal(get_percentile90(c(5, 2, 13, 2, 4)), 9.8)
   expect_equal(get_percentile90(c(NA, 10, NA)), 10)
@@ -54,7 +60,8 @@ test_that("get_percentile90", {
 })
 
 # Test get_median()
-test_that("get_median", {
+context("get_median")
+test_that("get_median works", {
   expect_equal(get_median(c(1, 2, 3, 4)), median(c(1, 2, 3, 4)))
   expect_equal(get_median(c(5, 2, 13, 2, 4)), median(c(5, 2, 13, 2, 4)))
   expect_equal(get_median(c(NA, 10, NA)), median(c(10)))
@@ -62,7 +69,8 @@ test_that("get_median", {
 })
 
 # Test get_average()
-test_that("get_average", {
+context("get_average")
+test_that("get_average works", {
   expect_equal(get_average(c(1, 2, 3, NA, 4)), 10/4)
   expect_equal(get_average(c(5, 2, 13, 2, 4)), 26/5)
   expect_equal(get_average(c(NA, 10, NA)), 10)
@@ -70,7 +78,8 @@ test_that("get_average", {
 })
 
 # Test get_stdev()
-test_that("get_stdev", {
+context("get_stdev")
+test_that("get_stdev works", {
   expect_equal(get_stdev(c(1, 2, 3, NA, 4)), sd(c(1, 2, 3, 4)))
   expect_equal(get_stdev(c(5, 2, 13, 2, 4)), sd(c(5, 2, 13, 2, 4)))
   expect_equal(get_stdev(c(NA, 10, NA)), sd(c(10)))
@@ -78,7 +87,8 @@ test_that("get_stdev", {
 })
 
 # Test get_quartile1()
-test_that("get_quartile1", {
+context("get_quartile1")
+test_that("get_quartile1 works", {
   expect_equal(get_quartile1(c(1, 2, 3, NA, 4)), 1.75)
   expect_equal(get_quartile1(c(5, 2, 13, 2, 4)), 2)
   expect_equal(get_quartile1(c(NA, 10, NA)), 10)
@@ -86,7 +96,8 @@ test_that("get_quartile1", {
 })
 
 # Test get_quartile3()
-test_that("get_quartile3", {
+context("get_quartile3")
+test_that("get_quartile3 works", {
   expect_equal(get_quartile3(c(1, 2, 3, NA, 4)), 3.25)
   expect_equal(get_quartile3(c(5, 2, 13, 2, 4)), 5)
   expect_equal(get_quartile3(c(NA, 10, NA)), 10)
@@ -94,7 +105,8 @@ test_that("get_quartile3", {
 })
 
 # Test count_missing()
-test_that("count_missing", {
+context("count_missing")
+test_that("count_missing works", {
   expect_equal(count_missing(c(1, 2, 3, NA, 4)), 1)
   expect_equal(count_missing(c(5, 2, 13, 2, 4)), 0)
   expect_equal(count_missing(c(NA, 10, NA)), 2)
@@ -102,7 +114,8 @@ test_that("count_missing", {
 })
 
 # Test summary_stats()
-test_that("summary_stats", {
+context("summary_stats")
+test_that("summary_stats works", {
   expect_equal(summary_stats(c(1, 2, 3, NA, 4))$mean, mean(c(1, 2, 3, 4)))
   expect_equal(summary_stats(c(5, 2, 13, 2, 4))$minimum, 2)
   expect_equal(summary_stats(c(NA, 10, NA))$missing, 2)
@@ -110,7 +123,8 @@ test_that("summary_stats", {
 })
 
 # Test rescale100()
-test_that("rescale100", {
+context("rescale100")
+test_that("rescale100 works", {
   expect_equal(rescale100(c(1, 2, 3, NA, 4), 0, 5), c(20, 40, 60, NA, 80))
   expect_equal(rescale100(c(5, 2, 13, 2, 4), 0, 100), c(5, 2, 13, 2, 4))
   expect_equal(rescale100(c(NA, 10, NA), 0, 10), c(NA, 100, NA))
@@ -118,7 +132,8 @@ test_that("rescale100", {
 })
 
 # Test drop_lowest()
-test_that("drop_lowest", {
+context("drop_lowest")
+test_that("drop_lowest works", {
   expect_equal(drop_lowest(c(1, 2, 3, NA, 4)), c(2,3, NA, 4))
   expect_equal(drop_lowest(c(5, 2, 13, 2, 4)), c(5, 13, 2, 4))
   expect_equal(drop_lowest(c(10,100)), c(100))
@@ -126,7 +141,8 @@ test_that("drop_lowest", {
 })
 
 # Test score_homework()
-test_that("score_homework", {
+context("score_homework")
+test_that("score_homework works", {
   expect_equal(score_homework(c(1, 2, 3, NA, 4), drop=TRUE), 3)
   expect_equal(score_homework(c(1, 2, 3, NA, 4), drop=FALSE), 10/4)
   expect_equal(score_homework(c(10,100), drop=FALSE), 110/2)
@@ -134,7 +150,8 @@ test_that("score_homework", {
 })
 
 # Test score_quiz()
-test_that("score_quiz", {
+context("score_quiz")
+test_that("score_quiz works", {
   expect_equal(score_quiz(c(1, 2, 3, NA, 4), drop=TRUE), 3)
   expect_equal(score_quiz(c(1, 2, 3, NA, 4), drop=FALSE), 10/4)
   expect_equal(score_quiz(c(10,100), drop=FALSE), 110/2)
@@ -142,7 +159,8 @@ test_that("score_quiz", {
 })
 
 # Test score_lab()
-test_that("score_lab", {
+context("score_lab")
+test_that("score_lab works", {
   expect_equal(score_lab(12), 100)
   expect_equal(score_lab(11), 100)
   expect_equal(score_lab(7), 20)
